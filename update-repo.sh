de77390d500beb47005316a8ef079842a66a6229
sarevalo3htp <sarevalo@3htp.com>
2023-12-14 15:37:03 -0500

    Ultima actualizacion html y python

--- update-repo.sh
+++ update-repo.sh
@@ -3,6 +3,13 @@
 # Ruta al repositorio local
 repo_local="/c/Users/sergi/Desktop/LEGOPS"
 
+# Ruta al archivo página.html
+archivo_html="pagina.html"
+
 # Actualizar el repositorio local
 cd $repo_local
 git pull origin main
+
+# Reiniciar el servidor local
+pkill -f "python -m http.server 8080"  # Detener el servidor existente
+python -m http.server 8080 &  # Iniciar el servidor en segundo plano
